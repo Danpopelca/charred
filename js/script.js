124b85a862498f106337053e0659a2843378769b
Danpopelca <119097088+Danpopelca@users.noreply.github.com>
2025-04-01 22:09:19 -0400

    create page for about/careers/contact

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ document.addEventListener("DOMContentLoaded", function () {
       video.playbackRate = 0.25; // 0.5 = de 2x mai lent (slow motion)
     }
   });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("menuImageModal");
+    const modalTitle = document.getElementById("modalDishTitle");
+    const modalImage = document.getElementById("modalDishImage");
+  
+    if (modal) {
+      modal.addEventListener("show.bs.modal", function (event) {
+        const trigger = event.relatedTarget;
+        const title = trigger.getAttribute("data-title");
+        const imgSrc = trigger.getAttribute("data-img");
+  
+        modalTitle.textContent = title;
+        modalImage.src = imgSrc;
+        modalImage.alt = title;
+      });
+    }
+  });
